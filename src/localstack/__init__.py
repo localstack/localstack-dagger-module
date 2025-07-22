cff10bb52806d65f39567f93158a014ce16f7915
Jason McCallister <jason@mccallister.dev>
2025-07-22 14:49:11 -0400

    Add docs to __init__.py
    Signed-off-by: Jason McCallister <jason@mccallister.dev>

--- src/localstack/__init__.py
+++ src/localstack/__init__.py
@@ -1,1 +1,8 @@
+"""LocalStack module for managing LocalStack instances and state.
+
+This module provides functions to start LocalStack containers, manage state through Cloud Pods,
+and handle ephemeral LocalStack instances in the cloud using LocalStack Cloud and an auth token.
+This module supports both Community and Pro versions of LocalStack.
+"""
+
 from .main import Localstack as Localstack
